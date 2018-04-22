7909b2ab7bd099db27a77bb34286fedc52cc94e7
Unknown <callahca200@potsdam.edu>
2018-04-22 09:07:11 -0400

    waluigi

--- trees.py
+++ trees.py
@@ -1,11 +1,37 @@
 import json
 class Trees:
+  
+  def inOrder(self, tree, n, list2) 
+     if n == len(tree)/2 + 1:
+         list2.add(tree[n])
+     if n == len(tree/2) + 2:
+         list2.add(tree[n])  
+     else: 
+        
+         
 
-  def traversal(self, tree, order):
+ def traversal(self, tree, order):
     result = {}
     # tree is an array. Look up how trees can be represented as arrays and how
     # you can do a traversal on it!
     # order can be "preOrder", "postOrder", or "inOrder"
+    #binary tree- is it a binary search tree
+    if order == 'preOrder'
+        result['result'] = true 
+        steps = []
+        for i in tree
+            steps.append({"node",i})
+        result['steps'] = steps
+    
+    if order == 'inOrder'
+        result['results'] = true
+        list1 = []
+        n = len(tree)
+        list2 = []
+        list1 = inOrder(tree, n, list2)
+        result['steps'] =  
+
+        
     # check which it is, and do the appropriate traversal
     # this method should return the following dictionary:
     #{
@@ -19,3 +45,5 @@ class Trees:
     # where each number is the node's index in the input array as you view
     # it in the traversal
     return json.loads(result)
+
+print(Trees.traversal([1, 2, 3, 4, 5], 'preOrder'))
