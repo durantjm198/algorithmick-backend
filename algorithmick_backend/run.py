8798cb65947cdd0f66157ab6b256f3f0402a1ff0
durantjm198 <durantjm198@potsdam.edu>
2018-04-26 21:34:30 -0400

    debug shit

--- algorithmick_backend/run.py
+++ algorithmick_backend/run.py
@@ -11,7 +11,7 @@ def hello():
 @application.route("/trees/traversal", methods=['POST'])
 def traversal():
   data = request.get_json()
-  print(data)
+  print(request.headers())
   order = data.get('traversal')
   tree = data.get('tree')
   return json.loads(Trees.traversal(tree, order))
