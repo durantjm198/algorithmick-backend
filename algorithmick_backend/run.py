7a7c8a8cb4b7e4c9c6f2c635c4ec673bfd6dbfd7
Jarred Durant <jarreddurant14@gmail.com>
2018-04-27 13:55:13 -0400

    add CORS headers

--- algorithmick_backend/run.py
+++ algorithmick_backend/run.py
@@ -4,6 +4,13 @@ from .trees import binary_tree_traversal
 
 application = Flask(__name__)
 
+@application.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 @application.route('/')
 def hello():
   return "<h1>KENDRICK DESERVED THE GRAMMY</hi>"
@@ -14,3 +21,5 @@ def traversal():
   order = data.get('traversal')
   tree = data.get('tree').split(', ')
   return json.dumps(binary_tree_traversal(tree, order))
+
+  
