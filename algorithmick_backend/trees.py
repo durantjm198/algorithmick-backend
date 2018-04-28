ecbe706e5746ca8fd5b1559d682130524de7d853
Jarred Durant <jarreddurant14@gmail.com>
2018-04-28 17:03:43 -0400

    smoke weed every day

--- algorithmick_backend/trees.py
+++ algorithmick_backend/trees.py
@@ -1,54 +1,23 @@
 import json
 
-#  def inOrder(self, tree, n, list2) 
-#    if n == len(tree)/2 + 1:
-#      list2.append(tree[n])
-#    if n == len(tree/2) + 2:
-#      list2.append(tree[n])  
-#     else: 
-      
-#  def postOrder(self, tree, root, steps):
-#    if tree[2 * root + 1] != None:
-#      postOrder(tree, 2 * root + 1)
-#    if tree[2 * root + 2] != 1
-#      postOrder(tree, 2 * root + 2)
-#    steps.append({"node" : root})
-#    return steps
-  
+def inOrderHelper(tree, root, steps):
+  if tree[2 * root] != None:
+    inOrderHelper(tree, root * 2, steps)
+  steps.append({'node': root})
+  if tree[2 * root + 1] != None:
+    inOrderHelper(tree, 2 * root + 1, steps)
+
 def binary_tree_traversal(tree, order='preOrder'):
   result = {}
+  result['steps'] = []
+
   if order == 'preOrder':
-    result["result"] = True
-    steps = []
     for i in tree:
-      steps.append({"node": i})
-    result["steps"] = steps
-    result["array"] = tree
-  print(str(result))
-  
-#   if order == 'inOrder'
-#      result['results'] = true
-  #     list1 = []
-  #    n = len(tree)
-    #  list2 = []
-    #   list1 = inOrder(tree, n, list2)
-      # result['steps'] =  
+      result["steps"].append({"node": i})
+  elif order == 'inOrder':
+    inOrderHelper(tree, tree[0], result['steps'])
+  #  elif order == 'postOrder':
 
-#    if order == 'postOrder'
-#     result['results'] = True
-#    result['steps'] = postOrder(tree, 0, [])
-
-      
-  # check which it is, and do the appropriate traversal
-  # this method should return the following dictionary:
-  #{
-  #  "result": true,
-  #  "steps": [
-  #    {"node": 0}, // these node numbers are the indices in the input array!
-  #    {"node": 1},
-  #    {"node": 3}
-  #  ]
-  #}
-  # where each number is the node's index in the input array as you view
-  # it in the traversal
+  result["array"] = tree  
+  result["result"] = True
   return result
