ce73a39f26b12dd82224b3aa37ca9788539452f3
durantjm198 <durantjm198@potsdam.edu>
2018-04-29 14:26:02 -0400

    add new helperssss

--- algorithmick_backend/trees.py
+++ algorithmick_backend/trees.py
@@ -1,5 +1,14 @@
 import json
 
+
+def preOrderHelper(tree, root, steps):
+  if tree[root] != None:
+    steps.append({'node': root})
+  if len(tree) > root * 2 + 1:
+    preOrderHelper(tree, root * 2 + 1, steps)
+  if len(tree) > root * 2 + 2:
+    preOrderHelper(tree, 2 * root + 2, steps)
+
 def inOrderHelper(tree, root, steps):
   if len(tree) > root * 2 + 1:
     inOrderHelper(tree, root * 2 + 1, steps)
@@ -8,16 +17,24 @@ def inOrderHelper(tree, root, steps):
   if len(tree) > root * 2 + 2:
     inOrderHelper(tree, 2 * root + 2, steps)
 
+def postOrderHelper(tree, root, steps):
+  if len(tree) > root * 2 + 1:
+    postOrderHelper(tree, root * 2 + 1, steps)
+  if len(tree) > root * 2 + 2:
+    postOrderHelper(tree, 2 * root + 2, steps)
+  if tree[root] != None:
+    steps.append({'node': root})
+  
 def binary_tree_traversal(tree, order='preOrder'):
   result = {}
   result['steps'] = []
 
   if order == 'preOrder':
-    for i in tree:
-      result["steps"].append({"node": i})
+    preOrderHelper(tree, 0, result['steps'])
   elif order == 'inOrder':
     inOrderHelper(tree, 0, result['steps'])
-  #  elif order == 'postOrder':
+  elif order == 'postOrder':
+    postOrderHelper(tree, 0, result['steps'])
 
   result["array"] = tree  
   result["result"] = True
