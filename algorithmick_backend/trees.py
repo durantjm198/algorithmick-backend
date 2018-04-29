041a0cd1a676a19545ca91d609dd536c8561900b
Jarred Durant <jarreddurant14@gmail.com>
2018-04-29 10:52:43 -0400

    +1+1+1+1

--- algorithmick_backend/trees.py
+++ algorithmick_backend/trees.py
@@ -2,12 +2,10 @@ import json
 
 def inOrderHelper(tree, root, steps):
   if len(tree) >= 2 * root + 1:
-    if tree[2 * root + 1] != None:
-      inOrderHelper(tree, root * 2, steps)
+    inOrderHelper(tree, root * 2 + 1, steps)
   steps.append({'node': root})
   if len(tree) >= 2 * root + 2:
-    if tree[2 * root + 2] != None:
-      inOrderHelper(tree, 2 * root + 1, steps)
+    inOrderHelper(tree, 2 * root + 2, steps)
 
 def binary_tree_traversal(tree, order='preOrder'):
   result = {}
